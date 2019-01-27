b79c75985b9fb7e69225f7c9a816f5013fa52ca6
4lorelei <43756481+4lorelei@users.noreply.github.com>
2019-01-27 02:07:26 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -5076,7 +5076,7 @@ function prossimo_aiuto($tempo_attesa, $data_livello, $data_sleep, $data_go)
 	$secondi_corr=strtotime($data_corr_new);	
 	
 	// Se secondi_break è valido e il livello è stato raggiunto prima di data_sleep
-	if (($secondi_go - $secondi_sleep) > 0 && ($secondi_sleep > $secondi_corr))
+	if (($secondi_go - $secondi_sleep) > 0 && ($secondi_sleep > $secondi))
 	    $secondi_break = $secondi_go - $secondi_sleep;
 	else 
 	    $secondi_break = 0;
